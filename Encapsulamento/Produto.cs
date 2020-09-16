aeab7d13911438a0e4accc7cb0b4b541647fc665
David Moraes <investtraderrs@gmail.com>
2020-09-16 18:21:23 -0300

    Conceito de Encapsulamento

---
+++ Encapsulamento/Produto.cs
@@ -0,0 +1,64 @@
+﻿using System.Globalization;
+namespace Course
+{
+    class Produto
+    {
+        private string _nome; //Esse modelo de Encapsulamento consiste em esconder os detalhes de implementação dos componentes do sistema, expondo apenas oerações seguras e que o mantenha em um estado consistente.
+        private double _preco; //Todo atributo é definido como priveate, para não poder ser acessado e modificado por outra classe do programa, sendo só possível alterar pelos métodos definidos.
+        private int _quantidade;
+        //REGRA DE OURO: O objeto(componente) deve sempre estar em um estado consistente, e a própria classe deve garantir isso.
+        public Produto()
+        {
+        }
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+        //Implementa-se métodos GET e SET para cada atributo, conforme regras de negócio.
+        //OBS. não é usual na plataforma C#, pois podemos utilizar as properts ou PROPRIEDADES para realizar esse feito de forma direta e resumida.
+        //Esses métodos abaixo é apenas didático para aprendizado do conceito.
+        public string GetNome()
+        {
+            return _nome;
+        }
+        public void SetNome(string nome)
+        {
+            if (nome != null && nome.Length > 1)
+            {
+                _nome = nome;
+            }
+        }
+        public double GetPreco()
+        {
+            return _preco;
+        }
+        public int GetQuantidade()
+        {
+            return _quantidade;
+        }
+        public double ValorTotalEmEstoque()
+        {
+            return _preco * _quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            _quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            _quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return _nome
+            + ", $ "
+            + _preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + _quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Encapsulamento/Program.cs
@@ -0,0 +1,23 @@
+﻿using Course;
+using System;
+using System.Globalization;
+
+namespace Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Produto p = new Produto("TV", 500.00, 10);
+
+            //(p.GetQuantidade = -10;) - Se não fosse colocado o encapsulamento(private) nos atributos da classe PRODUTO o usuário poderia utilizar esse código livremente para alterar a quantidade em estoque do produto sem precisar utilizar o método ADICIONARPRODUTO ou GetQUANTIDADE.
+            p.AdicionarProdutos(35); //Com o encapsulamento apenas utilizando os métodos definidos para conseguir alterar a quantidade em estoque.
+            p.RemoverProdutos(25);
+
+            Console.Write("Quantidade em estoque: " + p.GetQuantidade());
+            Console.WriteLine();
+        }
+    }
+}
